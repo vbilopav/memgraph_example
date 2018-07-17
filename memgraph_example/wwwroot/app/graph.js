ca2d2dbe8b65ce63236f1534949b5395a432818a
Vedran Bilopavlović <vbilopav@gmail.com>
2018-07-17 19:39:36 +0200

    add connecting lines

--- memgraph_example/wwwroot/app/graph.js
+++ memgraph_example/wwwroot/app/graph.js
@@ -23,7 +23,10 @@ define(["http://d3js.org/d3.v3.min.js"], () => class {
                     if (ids[value.id]) {
                         continue;
                     }
-                    nodes.push(Object.assign(value.properties, { nodeId: value.id, label: value.labels.join(", ") }));
+                    nodes.push(Object.assign(value.properties, {
+                        nodeId: value.id, 
+                        label: value.labels.join(", ")
+                    }));
                     ids[value.id] = true;
                 }
             }
@@ -40,50 +43,112 @@ define(["http://d3js.org/d3.v3.min.js"], () => class {
         d3.select("svg").remove();
 
         const
-            canvas = d3.select(this.element).append("svg")
+            svg = d3.select(this.element).append("svg")
                 .attr("width", "100%")
-                .attr("height", "100%")
-                .append("g");
-
+                .attr("height", "100%"),
+            canvas = svg.append("g");
+/*
+        svg.append("svg:defs").selectAll("marker")
+              .enter().append("svg:marker")
+                .attr("id", String)
+                .attr("viewBox", "0 -5 10 10")
+                .attr("refX", 15)
+                .attr("refY", -1.5)
+                .attr("markerWidth", 6)
+                .attr("markerHeight", 6)
+                .attr("orient", "auto")
+              .append("svg:path")
+                .attr("d", "M0,-5L10,0L0,5");
+*/
         let i = 0;
-        const
-            node = canvas.selectAll("g")
-                .data(nodes)
-                .enter()
-                .append("g")
-                .attr("id", d => "node-" + d.nodeId)
-                .attr("transform", d => {
-                    let x = 10 + ((boxWidth - 30) * i++);
-                    let y = 50 + ((boxHeight + 30) * (i % 3));
-                    return "translate(" + x + ", " + y + ")";
-                })
-                .on({
-                    "mouseover": () => node.style("cursor", "move"),
-                    "mouseout": () => node.style("cursor", "default"),
-                })
-                .call(d3.behavior.drag().origin(Object).on("drag", function () {
-                    const
-                        translate = (x, y) => Object({ x: x, y: y }),
-                        n = d3.select(this),
-                        r = eval(n.attr("transform"));
-                    n.attr("transform", "translate(" + (r.x + d3.event.dx) + "," + (r.y + d3.event.dy) + ")");
-                }));
+        const node = canvas.selectAll("g")
+            .data(nodes)
+            .enter()
+            .append("g")
+            .attr("class", "node")
+            .attr("id", d => "node-" + d.nodeId)
+            .attr("transform", d => {
+                let x = 100 + ((boxWidth - 10) * i++);
+                let y = 100 + ((boxHeight + 100) * (i % 3));
+                return "translate(" + x + ", " + y + ")";
+            })
+            .on({
+                "mouseover": () => node.style("cursor", "move"),
+                "mouseout": () => node.style("cursor", "default"),
+            })
+            .call(d3.behavior.drag().origin(Object).on("drag", function () {
+                const 
+                    n = d3.select(this), 
+                    [tx, ty] = d3.transform(n.attr("transform")).translate,
+                    x = tx + d3.event.dx,
+                    y = ty + d3.event.dy;
+                n.attr("transform", "translate(" + x + "," + y + ")");
 
-        node.append("rect")
+                const data = n.data()[0];
+                for(let l of d3.selectAll(".path-" + data.nodeId)[0]) {
+                    let line = d3.select(l), lineData = line.data()[0];
+                    if (lineData.startNodeId === data.nodeId) {
+                        line.attr("x1", x);
+                        line.attr("y1", y);
+                    } else if (lineData.endNodeId === data.nodeId) {
+                        line.attr("x2", x);
+                        line.attr("y2", y);
+                    }
+                }
+            }));
+
+        node
+            //.append("rect")
+            .append("circle").attr('r', 40) 
             .attr('class', 'memgraphNode')
             .attr("width", boxWidth)
             .attr("height", boxHeight);
 
         node.append("text")
             .attr("class", "memgraphTitle")
-            .attr("x", 44).attr("y", 17).attr("text-anchor", "middle")
+            //.attr("x", 44).attr("y", 17)
+            .attr("x", 0).attr("y", -10)
+            .attr("text-anchor", "middle")
             .text(d => d.name);
 
         node.append("text")
             .attr("class", "memgraphLabel")
-            .attr("x", 44).attr("y", 30)
+            //.attr("x", 44).attr("y", 30)
+            .attr("x", 0).attr("y", 10)
             .attr("text-anchor", "middle")
             .text(d => d.label);
+
+        canvas
+            .selectAll("line")
+            .data(relationships)
+            .enter()
+            .insert("line", ".node")
+            .attr("interpolate", "cardinal")
+            .style("stroke", "black")
+            .attr("id", d => "path-" + d.startNodeId + "-" + d.endNodeId)
+            .attr("class", d => "path-" + d.startNodeId + " " + "path-" + d.endNodeId)
+            .attr("data", d => {
+                let [x1, y1] = d3.transform(d3.select("#node-" + d.startNodeId).attr("transform")).translate;
+                let [x2, y2] = d3.transform(d3.select("#node-" + d.endNodeId).attr("transform")).translate;
+                d.x1 = x1;// + (boxWidth / 2);
+                d.y1 = y1;// + (boxHeight / 2);
+                d.x2 = x2;// + (boxWidth / 2);
+                d.y2 = y2;// + (boxHeight / 2);
+            })
+            .attr("x1", d => d.x1)
+            .attr("y1", d => d.y1)
+            .attr("x2", d => d.x2)
+            .attr("y2", d => d.y2);
+
+        canvas
+            .selectAll("#textPath")
+            .data(relationships)
+            .enter()
+            .insert("text", ".node")
+            .attr("id", "textPath")
+            .append("textPath")
+            .attr("xlink:href",  d => "#path-" + d.startNodeId + "-" + d.endNodeId)
+            .text("letters");
     }
 
 });
